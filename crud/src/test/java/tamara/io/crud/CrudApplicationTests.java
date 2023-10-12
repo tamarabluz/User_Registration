130b7b35c6b99693044646b7959eb4ba85f457c2
Tamara <tamarabluz@gmail.com>
2023-10-12 17:13:06 -0300

    first commit

---
+++ crud/src/test/java/tamara/io/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package tamara.io.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
